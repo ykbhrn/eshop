740bae9c24824ec1a87afbd2bc22e587d7de1d9e
ykbhrn <jakub.horun@gmail.com>
2021-12-07 03:30:23 +0000

    discount popup mostly done

--- frontend/src/components/common/PopupDiscount.js
+++ frontend/src/components/common/PopupDiscount.js
@@ -5,25 +5,40 @@ import { getAllDiscounts } from '../../lib/api'
 class PopupDiscount extends React.Component {
   state = {
     some: null,
-    showDiscount: false,
-    discountCounter: 0,
     discountTime: null
   }
 
   async componentDidMount () {
+    const res = await getAllDiscounts()
+    const now = new Date()
+    this.setState({discountTime: res.data[0].time})
+
+    if (now.getMinutes() === res.data[0].time) {
+      console.log("first")
+      this.showHand()
+      setTimeout(() => {
+        const discountPage = document.querySelector('.discount-page')
+        const discountPopup = document.querySelector('.discount-popup-wrapper')
+
+        discountPopup.style.visibility = "hidden"
+        discountPage.classList.remove("show-discount")
+      }, 30000);
+    } 
+
     setInterval(() => {
       const now = new Date()
-      if (now.getMinutes() === this.state.discountTime && this.state.discountCounter < 1) {
-        this.setState({showDiscount: true, discountCounter: this.state.discountCounter + 1})
-        console.log(this.state.discountCounter)
-      } else if (now.getMinutes() !== this.state.discountTime && this.state.discountCounter < 1) {
-        this.setState({showDiscount: false})
-        this.changeDiscountTime()
-        console.log(this.state.discountCounter, "else")
-      } else if (now.getMinutes() !== this.state.discountTime && this.state.discountCounter === 1) {
-        this.setState({showDiscount: false, discountCounter: this.state.discountCounter + 1})
-        console.log(this.state.discountCounter, "else another")
-      } 
+      if (now.getMinutes() === this.state.discountTime) {
+        console.log("first")
+        this.showHand()
+        setTimeout(() => {
+          const discountPage = document.querySelector('.discount-page')
+          const discountPopup = document.querySelector('.discount-popup-wrapper')
+
+          discountPopup.style.visibility = "hidden"
+          discountPage.classList.remove("show-discount")
+        }, 30000);
+      }
+      this.changeDiscountTime()
     }, 60000)
   }
 
@@ -36,21 +51,48 @@ class PopupDiscount extends React.Component {
     }
   }
 
+  showHand = () => {
+    const discountPage = document.querySelector('.discount-page')
+    const discountPopup = document.querySelector('.discount-popup-wrapper')
+    const availableHeight = window.innerHeight
+    const availableWidth = window.innerWidth
+
+    discountPopup.style.visibility = "visible"
+
+    window.setInterval(() => {
+      const positionX = Math.floor(Math.random() * availableWidth)
+      const positionY = Math.floor(Math.random() * availableHeight)
+
+      discountPopup.style.left = positionX + 'px'
+      discountPopup.style.top = positionY + 'px'
+    }, 1000);
+    discountPage.classList.add("show-discount")
+  }
+
   slap = () => {
-    const discount = document.querySelector('.discount-popup-wrapper')
-    discount.style.animation = "cursor 0.2s linear"
+    const discountPage = document.querySelector('.discount-page')
+
+    discountPage.classList.add("show-discount")
+    discountPage.style.animation = "cursor 0.2s linear"
     setTimeout(() => {
-      discount.style.animation = "none"
+      discountPage.style.animation = "none"
     }, 210);
+  }
+
+  slapped = () => {
+    const discountPopup = document.querySelector('.discount-popup-wrapper')
+
+    discountPopup.style.transform = "rotate3d(1, 0, 1, 70deg)"
+    // setTimeout(() => {
+    //   discountPopup.style.transform = "none"
+    // }, 210);
   }
 
   render() {
     return (
-      <div>
-        {/* {this.state.showDiscount && */}
-        <div className="discount-popup-wrapper" onClick={this.slap}>
+      <div className="discount-page" onClick={this.slap}>
+        <div className="discount-popup-wrapper" onClick={this.slapped}>
         </div>
-        {/* // } */}
       </div>
     )
   }
