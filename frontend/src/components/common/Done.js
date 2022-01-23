5fdbb6a52a52ee96040150ceee35c2d3fff35c99
ykbhrn <jakub.horun@gmail.com>
2022-01-23 18:00:27 +0000

    app deployed, responsiveness fixed,

--- frontend/src/components/common/Done.js
+++ frontend/src/components/common/Done.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import { getMyProfile } from '../../lib/api'
 
 class Done extends React.Component {
   state = {
+    user: null,
     redirect: false,
     progress: 100,
-    timeBeforeRedirect: 3000,
+    timeBeforeRedirect: 5000,
   }
 
-  componentDidMount() {
-    //After five seconds to fire render redirect
-    setTimeout(() => this.setState({ redirect: true }), this.state.timeBeforeRedirect)
-    return  setInterval(() => this.progressReducer(this.state.progress), this.state.timeBeforeRedirect / 100)
+  async componentDidMount() {
+    try {
+      window.scrollTo(0, 0)
+      const res = await getMyProfile()
+      //After five seconds to fire render redirect
+      this.setState({ user: res.data })
+      setTimeout(() => this.setState({ redirect: true }), this.state.timeBeforeRedirect)
+      return  setInterval(() => this.progressReducer(this.state.progress), this.state.timeBeforeRedirect / 100)
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   progressReducer = (currentNum) => {
@@ -28,12 +37,13 @@ class Done extends React.Component {
   }
 
   render() {
+    if (!this.state.user) return null
     return (
       <div className="done-page">
         {this.renderRedirect()}
         <div className="progress-wrapper">
-          <h1 className="title is-1"> 
-            Your acount was created. Welcome
+          <h1> 
+            Your acount was created. Welcome {this.state.user.name}
           </h1>
           <progress value={this.state.progress} max="100"></progress>
         </div>
