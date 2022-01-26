ecac8d5cc9f7edf4aa160858bd095149217dfcde
ykbhrn <jakub.horun@gmail.com>
2022-01-26 04:19:55 +0000

    s

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,3 @@
-require('dotenv').config()
-
 const port = process.env.PORT || 8000
 const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/eshop-database2'
-'muie'
 module.exports = { port, dbURI }
